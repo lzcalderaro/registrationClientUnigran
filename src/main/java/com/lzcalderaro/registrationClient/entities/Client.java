db204af2eb61f84ddb5cb40e597847839dd70776
Luiz Calderaro <lzcalderaro@gmail.com>
2024-10-18 17:09:11 +0100

    Atividade 07

--- src/main/java/com/lzcalderaro/registrationClient/entities/Client.java
+++ src/main/java/com/lzcalderaro/registrationClient/entities/Client.java
@@ -2,10 +2,10 @@ package com.lzcalderaro.registrationClient.entities;
 
 public final class Client {
 
-    private final String name;
-    private final String email;
-    private final String identityNumber;
-    private final String mobileNumber;
+    private String name;
+    private String email;
+    private String identityNumber;
+    private String mobileNumber;
     private Integer id;
 
     public Client(String name, String email, String identityNumber, String mobileNumber) {
@@ -36,4 +36,9 @@ public final class Client {
     public String getIdentityNumber() {return identityNumber;}
     public String getMobileNumber() {return mobileNumber;}
     public Integer getId() {return id;}
+
+    public void setEmail(String email) {this.email = email;}
+    public void setIdentityNumber(String identityNumber) {this.identityNumber = identityNumber;}
+    public void setMobileNumber(String mobileNumber) {this.mobileNumber = mobileNumber;}
+    public void setName(String name) {this.name = name;}
 }
